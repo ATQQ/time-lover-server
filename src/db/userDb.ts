9a844a6f56f4230d74305f83a6307c339a2e7475
sugar <engineerzjl@foxmail.com>
2021-06-15 16:50:22 +0800

    Initial commit

---
+++ src/db/userDb.ts
@@ -0,0 +1,10 @@
+import { query } from '@/lib/dbConnect/mysql'
+import { User } from '@/db/modal'
+import { selectTableByModel } from '@/utils/sqlUtil'
+
+export function selectUserByUsername(username: string): Promise<User[]> {
+    const { sql, params } = selectTableByModel('user', {
+        data: { username }
+    })
+    return query<User[]>(sql, ...params)
+}
---
+++ src/lib/dbConnect/mysql.ts
@@ -0,0 +1,47 @@
+import mysql from 'mysql'
+import { mysqlConfig } from '@/config'
+// 创建连接池
+const pool = mysql.createPool(mysqlConfig)
+
+export function getConnection(): Promise<mysql.PoolConnection> {
+    return new Promise((res, rej) => {
+        pool.getConnection((err, coon) => {
+            if (err) {
+                console.error('------ db connection error -------')
+                console.error(err)
+                rej(err)
+                return
+            }
+            res(coon)
+        })
+    })
+}
+
+pool.on('error', (err) => {
+    console.log('pool connect error')
+    console.error(err)
+})
+
+type param = string | number
+/**
+ * 执行sql语句
+ * @param sql sql语句
+ * @param params 参数
+ */
+export function query<T>(sql: string, ...params: param[]): Promise<T> {
+    return new Promise<T>((resolve, reject) => {
+        getConnection().then((coon) => {
+            coon.query(sql, params, (err, result) => {
+                if (err) {
+                    reject(err)
+                    return
+                }
+                // 请求完就释放
+                coon.release()
+                resolve(result)
+            })
+        }).catch((err) => {
+            reject(err)
+        })
+    })
+}
