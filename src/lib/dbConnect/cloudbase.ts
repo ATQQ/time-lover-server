155f66238f6784a4c570086345a97b11f2f781d6
ATQQ <engineerzjl@foxmail.com>
2021-06-15 20:01:45 +0800

    feat: 封装云数据库操作方法

---
+++ src/lib/dbConnect/cloudbase.ts
@@ -0,0 +1,23 @@
+import cloudbase from '@cloudbase/node-sdk'
+const app = cloudbase.init({
+    secretId: process.env.secretId,
+    secretKey: process.env.secretKey,
+    env: process.env.envId
+})
+
+export const db = app.database()
+export function insertDocument<T>(collection: string, data: T | T[]) {
+    return db.collection(collection).add(data)
+}
+
+export function deleteDocument(collection: string, query: any) {
+    return db.collection(collection).where(query).remove()
+}
+
+export function findDocument(collection: string, query: any) {
+    return db.collection(collection).where(query).get()
+}
+
+export function updateDocument<T>(collection: string, query: any, data:T) {
+    return db.collection(collection).where(query).update(data)
+}
