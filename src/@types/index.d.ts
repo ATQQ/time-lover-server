bae1590f5239c2527e9464f0f090ebd141b06ffb
ATQQ <engineerzjl@foxmail.com>
2021-06-17 13:50:00 +0800

    feat: 修改sls配置

---
+++ src/@types/index.d.ts
@@ -0,0 +1,3 @@
+// fix build error
+// node_modules/tencentcloud-sdk-nodejs/tencentcloud/services/cr/v20180321/cr_models.d.ts:1321:12 - error TS2304: Cannot find name 'File'.
+declare type File = any
